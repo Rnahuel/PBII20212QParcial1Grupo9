03fc1a6976000154389d75d456f36dfb1cc971f3
Maria Chiara Espinoza <mariachiaraespinoza@gmail.com>
2021-10-06 12:02:10 -0300

    prueba

--- src/ar/edu/unlam/dominio/Otro.java
+++ src/ar/edu/unlam/dominio/Otro.java
@@ -4,13 +4,13 @@ public class Otro extends Musico{
 	
 	private boolean cantaONo;
 	private String descripcion;
-	public Otro(Integer id, String nombre, String apellido, String nombreArtistico, Boolean tieneFormacionAcademica,
+	/*public Otro(Integer id, String nombre, String apellido, String nombreArtistico, Boolean tieneFormacionAcademica,
 			Boolean manejaMasDeUnInstrumento) {
 		
 		super(id, nombre, apellido, nombreArtistico, tieneFormacionAcademica, manejaMasDeUnInstrumento);
 		this.descripcion = descripcion;
 		this.cantaONo = cantaONo;
-	}
+	}*/
 	
 	
 }
